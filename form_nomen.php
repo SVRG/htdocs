646af49b95753f4dede0d69180a0fc8cdf83ab73
SVRG <tihomirov.sa@gmail.com>
2017-04-10 09:41:40 +0300

    Events

--- form_nomen.php
+++ form_nomen.php
@@ -3,6 +3,9 @@ $MM_authorizedUsers = "";
 $MM_donotCheckaccess = "true";
 
 include_once "security.php";
+include_once("class_elem.php");
+$E = new Elem();
+$E->Events();
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -20,19 +23,21 @@ include_once "security.php";
             <!--pagecell1-->
             <?php
 
-            include_once("class_elem.php");
-
             $UserG = array('admin', 'oper');
 
-            if (in_array($_SESSION['MM_UserGroup'], $UserG))
-                echo Func::ActButton('form_add_elem.php', 'Добавить Элемент');
+            $add = false;
+            if(isset($_POST['Flag']))
+                if($_POST['Flag']=='formAdd')
+            {
+                echo $E->formAddEdit();
+                $add = true;
+            }
 
-            $e = new Elem();
-            echo $e->ShowNomen();
-
-            if (in_array($_SESSION['MM_UserGroup'], $UserG))
-                echo Func::ActButton('form_add_elem.php', 'Добавить Элемент');
-
+            if(!$add)
+                if (in_array($_SESSION['MM_UserGroup'], $UserG)) {
+                    echo Func::ActButton('', 'Добавить Элемент', 'formAdd');
+                    echo $E->formNomen();
+                }
             ?>
         </div>
     </body>
