8ee3d7c7b1f16788f6dcf11e13bda059e7f328e2
SVRG <tihomirov.sa@gmail.com>
2017-08-17 14:49:05 +0300

    bugfix

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
-    <link rel="stylesheet" href="img/emx_nav_left.css" type="text/css"/>
+ <link rel="stylesheet" href="img/emx_nav_left.css" type="text/css"/>
     <div id="masthead">
         <blockquote class="glink">
             <div class="glink" id="utility">
-                Добро пожаловать: <?php echo func::user();
+                Добро пожаловать: <?php
+                include_once "class_func.php";
+                echo func::user();
                 echo "<br><a href=". $_SERVER['PHP_SELF'] . "?doLogout=true>Выход</a>";?>
             </div>
         </blockquote>
