9bc76e37e3858be057469134e44055a202fb23d8
SVRG <tihomirov.sa@gmail.com>
2017-08-23 16:15:59 +0300

    menu
     - новое упрощенное меню

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
- <link rel="stylesheet" href="img/emx_nav_left.css" type="text/css"/>
-    <div id="masthead">
-        <blockquote class="glink">
-            <div class="glink" id="utility">
-                Добро пожаловать: <?php
-                include_once "class_func.php";
-                echo func::user();
-                echo "<br><a href=". $_SERVER['PHP_SELF'] . "?doLogout=true>Выход</a>";?>
-            </div>
-        </blockquote>
+<link rel="stylesheet" type="text/css" href="menu/menu.css">
+<div>
+    <div class="glink" id="utility">
+        Добро пожаловать:
+        <?php
+            include_once "class_func.php";
+            echo func::user();
+            echo "<br><a href=" . $_SERVER['PHP_SELF'] . "?doLogout=true>Выход</a>";
+        ?>
     </div>
+</div>
 <?php include('menu/menu.php'); ?>
--- menu/menu.php
+++ menu/menu.php
@@ -1,37 +1,22 @@
-<script type="text/javascript" id="sothink_dhtmlmenu"> <!--
-    st_siteroot = "file:///C|/xampp/htdocs";
-    st_jspath = "/menu/stmenu.js";
-    if (!window.location.href.indexOf("file:") && st_jspath.charAt(0) === "/")
-        document.write('<script type="text/javascript" src="' + st_siteroot + st_jspath + '"><\/script>');
-    else
-        document.write('<script type="text/javascript" src="' + st_jspath + '"><\/script>');
-    //--> </script>
-<script type="text/javascript">
-    <!--
-    stm_bm(["menu1a91", 820, "/menu", "blank.gif", 1, "30", "0", 0, 0, 250, 0, 1000, 1, 0, 0, "", "", 67108897, 0, 1, 2, "hand", "hand", "file:///C|/xampp/htdocs"], this);
-    stm_bp("p0", [0, 4, 0, 0, 0, 7, 5, 0, 100, "", -2, "", -2, 50, 0, 0, "#999999", "transparent", "bg_01.gif", 3, 1, 1, "#000000"]);
-    stm_ai("p0i0", [0, "График Поставок", "", "", -1, -1, 0, "form_main.php?rplan=1", "_self", "", "", "", "", 5, 5, 0, "", "", -1, -1, 0, 1, 1, "#FFFFF7", 1, "#993333", 1, "", "bg_02.gif", 3, 1, 0, 0, "#FFFFF7", "#000000", "#FFFFFF", "#FFFFFF", "bold 9pt Arial", "bold 9pt Arial", 0, 0], 135, 0);
-    stm_bpx("p1", "p0", [1, 4, 0, 2, 0, 5, 0, 0, 80, "progid:DXImageTransform.Microsoft.RandomDissolve(,enabled=0,Duration=0.30)", 12, "progid:DXImageTransform.Microsoft.RandomDissolve(,enabled=0,Duration=0.30)", 12, 80, 0, 0, "#666666", "#000000", ""]);
-    stm_aix("p1i0", "p0i0", [0, "Детали по складу", "", "", -1, -1, 0, "form_main.php?sgp=1"], 135, 18);
-    stm_aix("p1i0", "p0i0", [0, "Оплачено", "", "", -1, -1, 0, "form_main.php?RP=1"], 135, 18);
-    stm_aix("p1i0", "p0i0", [0, "НЕТ ОПЛАТЫ", "", "", -1, -1, 0, "form_main.php?RPN=1"], 135, 18);
-    stm_aix("p1i0", "p0i0", [0, "ДОЛЖНИКИ", "", "", -1, -1, 0, "form_orglist.php?dolg"], 135, 18);
-    stm_aix("p1i0", "p0i0", [0, "Внешние", "", "", -1, -1, 0, "form_main.php?sgp=2"], 135, 18);
-    stm_ep();
-    stm_ai("p0i1", [6, 1, "#000000", "", -1, -1, 0]);
-    stm_aix("p0i2", "p0i0", [0, "Договоры", "", "", -1, -1, 0, "form_doclist.php", "_self", "", "", "", "", 5, 5, 0, "", "", 0, 0, 0, 1, 1, "#FFFFF7", 1, "#993333", 1, "", "bg_02.gif", 1], 135, 0);
-    stm_aix("p0i3", "p0i1", []);
-    stm_aix("p0i4", "p0i2", [0, "Организации", "", "", -1, -1, 0, "form_orglist.php"], 135, 0);
-    stm_aix("p0i5", "p0i1", []);
-    stm_aix("p0i6", "p0i2", [0, "Номенклатура", "", "", -1, -1, 0, "form_nomen.php"], 135, 0);
-    stm_aix("p0i7", "p0i1", []);
-    stm_aix("p0i8", "p0i2", [0, "Платежи", "", "", -1, -1, 0, "form_pays.php", "_self", "", "", "", "", 5, 5, 0, "", "", -1, -1], 135, 0);
-    stm_bpx("p2", "p1", []);
-    stm_ep();
-    stm_aix("p0i9", "p0i1", []);
-    stm_aix("p0i10", "p0i8", [0, "Склад", "", "", -1, -1, 0, "form_main.php?sgphistory=2"], 135, 0);
-    stm_ep();
-    stm_sc(1, ["transparent", "transparent", "", "", 3, 3, 0, 0, "#FFFFF7", "#000000", "up_disabled.gif", "up_enabled.gif", 7, 9, 0, "down_disabled.gif", "down_enabled.gif", 7, 9, 0, 0, 200]);
-    stm_em();
-    //-->
-</script>
+<div class="menu" id="drop-nav-menu">
+    <ul>
+        <li><a href="../form_main.php">График Поставок</a>
+            <ul>
+                <li><a href="../form_main.php?sgp=1">Детали по складу</a></li>
+                <li><a href="../form_main.php?sgp=5">Оплачено</a></li>
+                <li><a href="../form_main.php?sgp=4">Не Оплачено</a></li>
+                <li><a href="../form_orglist.php?dolg">Должники</a></li>
+                <li><a href="../form_main.php?sgp=2">Внешние</a></li>
+            </ul>
+        </li>
+        <li><a href="../form_doclist.php">Договоры</a></li>
+        <li><a href="../form_orglist.php">Организации</a></li>
+        <li><a href="../form_nomen.php">Номенклатура</a></li>
+        <li><a href="../form_pays.php">Платежи</a></li>
+        <li><a href="../form_main.php?sgp=3">Склад</a>
+            <ul>
+                <li><a href="../form_main.php?sgp=6">Отгрузка без оплаты</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
