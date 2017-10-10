fa3bc2ba7dff2b39e3be253641cdb34b4fff0883
SVRG <tihomirov.sa@gmail.com>
2017-10-10 11:50:05 +0300

    DOCTYPE
     - вернул удаленный DOCTYPE в формах

--- form_doclist.php
+++ form_doclist.php
@@ -10,6 +10,8 @@ $Dogovor = new Doc();
 $Dogovor->Events();
 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <!-- DW6 -->
 <head>
