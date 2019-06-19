184bc4e556e67058b9c4c2a0cfad6d9ff50a3bfa
SVRG <tihomirov.sa@gmail.com>
2019-06-19 11:23:27 +0300

    1C Bugfix
     - оказалось в новой версии 1С возникают текстовые сивмолы в коде, переделал таблицу и поправил импорт

--- import_data.php
+++ import_data.php
@@ -43,18 +43,18 @@ if (isset($_POST["Import"])) {
                 continue;
 
             $name = func::clearString($emapData[0]); // Удаляем лишние пробелы
-            $kod_1c = (int)$emapData[1];
+            $kod_1c = $emapData[1];
             $price = func::clearNum($emapData[2]);
             $numb = func::clearNum($emapData[3]);
             $sum = func::clearNum($emapData[4]);
 
             // Поверка данных
-            if ($name == "" or $kod_1c <= 0 or $price <= 0 or $numb <= 0 or $sum <= 0)
+            if ($name == "" or $kod_1c == "" or $price <= 0 or $numb <= 0 or $sum <= 0)
                 continue;
 
             $cnt++;
             $sql = /** @lang MySQL */
-                "INSERT INTO sklad_1c(name, kod_1c, price, numb, sum) VALUES ('$name',$kod_1c,$price,$numb,$sum);";
+                "INSERT INTO sklad_1c(name, kod_1c, price, numb, sum) VALUES ('$name','$kod_1c',$price,$numb,$sum);";
             $db->query($sql);
         }
 
