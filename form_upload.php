6c17f1ad4a9d6f2755471f0ce0001e753d43bcce
SVRG <tihomirov.sa@gmail.com>
2019-06-28 19:56:26 +0300

    Bugfix
     - добавлена проверка на наличие файл для загрузке. Ранее при отсутствии файла выдавалась ошибка, теперь форма перезагрузится и ничего не произойдет.

--- form_upload.php
+++ form_upload.php
@@ -30,50 +30,51 @@ elseif ($_GET['Desc'] == 'IncludeToOrg' and isset($_GET['kod_org'])) {
 $btn = ''; // Кнопка
 $CopyOK = false;
 
-if (isset($_FILES["filename"])) {
-    $dest = $_FILES["filename"]["tmp_name"];
+if (isset($_FILES["filename"]))
+    if ($_FILES["filename"]["tmp_name"] !== "") {
+        $dest = $_FILES["filename"]["tmp_name"];
 
-    $info = new SplFileInfo($_FILES["filename"]["name"]);
-    $ext = $info->getExtension();
+        $info = new SplFileInfo($_FILES["filename"]["name"]);
+        $ext = $info->getExtension();
 
-    $fname = Func::rus2lat2(Func::_strip($_FILES["filename"]["name"]));
+        $fname = Func::rus2lat2(Func::_strip($_FILES["filename"]["name"]));
 
-    $date = Func::NowDoc();
-    $rnd = rand();
+        $date = Func::NowDoc();
+        $rnd = rand();
 
-    $new_file_name = $date . '-' . $rnd . '-' . $fname . '.' . $ext;
+        $new_file_name = $date . '-' . $rnd . '-' . $fname . '.' . $ext;
 
-    $path = realpath($_SERVER["DOCUMENT_ROOT"]);
-    $path .= '/docs';
+        $path = realpath($_SERVER["DOCUMENT_ROOT"]);
+        $path .= '/docs';
 
-    if (copy($dest, $path . '/' . $new_file_name)) {
+        if (copy($dest, $path . '/' . $new_file_name)) {
 
-        if (file_exists($path . '/' . $new_file_name))
-            $CopyOK = true;
+            if (file_exists($path . '/' . $new_file_name))
+                $CopyOK = true;
 
-        $path = 'docs/' . $new_file_name;
-        $name = $_POST['Type'];
+            $path = 'docs/' . $new_file_name;
+            $name = $_POST['Type'];
 
-        if ($name == '')
-            $name = '*empty';
+            if ($name == '')
+                $name = '*empty';
 
-        $docum = new Docum();
+            $docum = new Docum();
 
 // ------------------------------------------------------------------------------
-        if ($_GET['Desc'] == 'IncludeToDoc' and isset($_GET['kod_dogovora'])) {
-            $docum->Add($name, $path, $_GET['kod_dogovora'], 'Doc');
-            $btn = Func::ActButton('form_dogovor.php?kod_dogovora=' . $_GET['kod_dogovora'], 'Перейти к Договору');
-        } // ------------------------------------------------------------------------------
-        elseif ($_GET['Desc'] == 'IncludeToElem' and isset($_GET['kod_elem'])) {
-            $docum->Add($name, $path, $_GET['kod_elem'], 'Elem');
-            $btn = Func::ActButton('form_elem.php?kod_elem=' . $_GET['kod_elem'], 'Перейти к Элементу');
-        } // ------------------------------------------------------------------------------
-        elseif ($_GET['Desc'] == 'IncludeToOrg' and isset($_GET['kod_org'])) {
-            $docum->Add($name, $path, $_GET['kod_org'], 'Org');
-            $btn = Func::ActButton('form_org.php?kod_org=' . $_GET['kod_org'], 'Перейти к Организации');
+            if ($_GET['Desc'] == 'IncludeToDoc' and isset($_GET['kod_dogovora'])) {
+                $docum->Add($name, $path, $_GET['kod_dogovora'], 'Doc');
+                $btn = Func::ActButton('form_dogovor.php?kod_dogovora=' . $_GET['kod_dogovora'], 'Перейти к Договору');
+            } // ------------------------------------------------------------------------------
+            elseif ($_GET['Desc'] == 'IncludeToElem' and isset($_GET['kod_elem'])) {
+                $docum->Add($name, $path, $_GET['kod_elem'], 'Elem');
+                $btn = Func::ActButton('form_elem.php?kod_elem=' . $_GET['kod_elem'], 'Перейти к Элементу');
+            } // ------------------------------------------------------------------------------
+            elseif ($_GET['Desc'] == 'IncludeToOrg' and isset($_GET['kod_org'])) {
+                $docum->Add($name, $path, $_GET['kod_org'], 'Org');
+                $btn = Func::ActButton('form_org.php?kod_org=' . $_GET['kod_org'], 'Перейти к Организации');
+            }
         }
     }
-}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
